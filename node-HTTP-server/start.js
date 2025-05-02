567941662ae296985e94921797e3428b3df5fe14
kavit-bokarvadiya <kavitit2000@gmail.com>
2025-05-03 01:48:25 +0530

    http-server-learn-and-prectice

--- node-HTTP-server/start.js
+++ node-HTTP-server/start.js
@@ -15,13 +15,49 @@
 //   }
 // });
 
-
-
+// task :: creation of log
 // creation of server with log :: in this server i have use the fs module
 
 const http = require("http");
 const fs = require("fs");
+const PORT = 8000;
 
-const myApp = http.createServer((req, res) => {
-    const log = "";
+// handler
+
+/**
+ *
+ *  req --> get client side meta data from client to server
+ *  res --> for response server to client
+ *
+ */
+
+const myServer = http.createServer((req, res) => {
+  const log = `${Date.now()}: ${req.url} new request recived \n`;
+  fs.appendFile("./log.txt", log, (err, data) => {
+    if (err) {
+      console.error("Error :: ", err);
+    } else {
+      switch (req.url) {
+        case "/":
+          res.end("home page");
+          break;
+        case "/about":
+          res.end("about page");
+          break;
+        case "/contactus":
+          res.end("contact us page");
+          break;
+        default:
+          res.end("404 NOT FOUND!");
+      }
+    }
+  });
+});
+
+myServer.listen(PORT, (err) => {
+  if (err) {
+    console.error("something won't to wrong!!");
+  } else {
+    console.log(`server is run on localhost :: ${PORT}`);
+  }
 });
